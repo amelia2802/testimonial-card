bee0044d5fdc755b151dcff158b810b0aa7ead7a
Amelia Dutta <amelia280111dutta@gmail.com>
2024-05-22 20:52:18 +0530

    complered testimonial card challenge

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import profilePicture from './img/profile-thumbnail.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App font-notoSans w-full h-full flex flex-col items-center ">
+      <section className='w-96 h-auto my-52 p-6 sm: w-80 bg-white rounded-xl'>
+        <section className='flex'>
+          <img className='w-14 h-14 mr-4' src={profilePicture} alt="profile avatar" />
+          <section>
+            <h1 className='text-lg font-semibold'>Sarah Dole</h1>
+            <p className='text-sm text-secondaryColor'>@sarahdole</p>
+          </section>
+        </section>
+        <p className='mt-4 '>I've been searching for high-quality abstract images for my design projects, 
+      and I'm thrilled to have found this platform. The variety and depth of creativity are astounding!</p>
+      </section>
+
+      <div class="absolute bottom-14 text-center" data-gfe-screenshot-exclude="true">
+      A challenge by
+      <a href="https://www.greatfrontend.com/projects?ref=challenges" target="_blank" rel="noreferrer"> GreatFrontEnd Projects</a>. Built by
+      <a
+        href="https://www.greatfrontend.com/projects/u/username"
+        target="_blank" rel="noreferrer"
+        > Amelia D</a>.
+    </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        customBg: 'linear-gradient(45deg, #F9FAFB 0%, #D2D6DB 100%)',
+      },
+      colors:{
+        secondaryColor:"#525252",
+      },
+      fontFamily:{
+        notoSans:["Noto Sans", "sans-serif"],
+      },
+      screens: {
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      }
+    },
+  },
+  plugins: [],
+}
+
